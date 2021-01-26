c1c827bc96398f4e6991dd20e63749ca9028fa4c
Bernardo Facchi <bernardobf2010@hotmail.com>
2021-01-26 15:29:48 -0300

    Build

--- build.py
+++ build.py
@@ -1,16 +1,16 @@
 import sys
 from cx_Freeze import setup, Executable
 
-includes = ["os", "pygame", "pygame.locals", "random", "pickle", "time", "json", "pyperclip", "sys", "reportlab", "fitz", "PyPDF2"]
+includes = ["os", "pygame", "pygame.locals", "random", "pickle", "time", "json", "pyperclip", "sys", "reportlab", "fitz", "PyPDF2", "win32api"]
 excludes = ["tkinter", "test"]
-include_files = ["config.json", "Functions.py", "Games.py", "./Buttons/", "./Fonts/", "./Layout/", "./Modules/", "./Quizzes/", "icon.png"]
+include_files = ["config.json", "Functions.py", "Games.py", "./Buttons/", "./Fonts/", "./Layout/", "./Modules/", "./Quizzes/", "icon.png", "Variables.py", "Quiz.pdf"]
 packages = []
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(  name = "Overthink",
-        version = "1.0",
-        options = {"build_exe": {"includes": includes, "excludes": excludes, "packages": packages, "include_files": include_files}},
-        executables = [Executable("Overthink.pyw", base=base, icon="icon.ico")])
+setup(name="Overthink",
+      version="1.0",
+      options={"build_exe": {"includes": includes, "excludes": excludes, "packages": packages, "include_files": include_files}},
+      executables=[Executable("Overthink.pyw", base=base, icon="icon.ico")])
