5a2556aa8f76763c70517eb4cc362f1a4dc2f6e1
Bernardo Facchi <bernardobf2010@hotmail.com>
2021-01-26 15:48:42 -0300

    LICENSE

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 
 includes = ["os", "pygame", "pygame.locals", "random", "pickle", "time", "json", "pyperclip", "sys", "reportlab", "fitz", "PyPDF2", "win32api"]
 excludes = ["tkinter", "test"]
-include_files = ["config.json", "Functions.py", "Games.py", "./Buttons/", "./Fonts/", "./Layout/", "./Modules/", "./Quizzes/", "icon.png", "Variables.py", "Quiz.pdf"]
+include_files = ["config.json", "Functions.py", "Games.py", "./Buttons/", "./Fonts/", "./Layout/", "./Modules/", "./Quizzes/", "icon.png", "Variables.py", "Quiz.pdf", "LICENSE"]
 packages = []
 
 base = None
